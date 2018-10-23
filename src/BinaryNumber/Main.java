78c83ddc0fdae56c4bb1783ff8f084d55dd7dcc6
wangyufei1006 <1650043869@qq.com>
2018-10-23 17:21:39 +0800

    :fire:

--- src/BinaryNumber/Main.java
+++ src/BinaryNumber/Main.java
@@ -6,13 +6,13 @@ package BinaryNumber;
  */
 
 public class Main {
-    public static int numberOfOne1(int n){
+    public static int numberOfOne1(int n) {
         //记录1的个数
         int result = 0;
 
         // JAVA语言规范中，int整形占四个字节，总计32位
         // 对每一个位置与1进行求与操作，再累加就可以求出当前数字的表示是多少位1
-        for (int i = 0;i < 32;i++){
+        for (int i = 0; i < 32; i++) {
             result += (n & 1);
             n >>>= 1;
         }
@@ -21,10 +21,10 @@ public class Main {
         return result;
     }
 
-    public static int numberOfOne2(int n){
+    public static int numberOfOne2(int n) {
         int result = 0;
 
-        while (n != 0){
+        while (n != 0) {
             result++;
 
             // 从最右边的1开始，每一次操作都使n的最右的一个1变成了0，
@@ -56,7 +56,7 @@ public class Main {
         int n = 10;
         String result = Integer.toBinaryString(n);//转换为二进制
         System.out.println(result);
-        int m = Integer.valueOf(result);
+        int m = Integer.valueOf(result);//转换为整数
         System.out.println(m);
         System.out.println(numberOfOne1(m));
     }
